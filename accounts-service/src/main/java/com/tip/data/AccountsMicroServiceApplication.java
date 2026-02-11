1b5d4eb172c455a59db41963bc1521e3fe8275f7
Haneef NB <haneef.noorbasha@gmail.com>
2026-02-11 00:26:39 -0600

    Add Account Micro Service

---
+++ accounts-service/src/main/java/com/tip/data/AccountsMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tip.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsMicroServiceApplication.class, args);
+		System.out.println("===Application Started===");
+	}
+
+}
